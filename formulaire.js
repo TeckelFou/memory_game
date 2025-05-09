a1314d81afc2dacb17a85bfddac7ddc542740a93
TeckelFou <laurine.groven@gmail.com>
2025-05-09 13:28:58 +0200

    ajout de fonctionnalités dans le formulaire

--- formulaire.js
+++ formulaire.js
@@ -1,104 +1,136 @@
 //récupérer et gère le formulaire, [ ] pour accéder à l'éelement par son nom
 function entryForm() {
-    //selection du formulaire
-    const form = document.forms["formulaire"];
-    //recup des champs
-    const userName = form["userName"];
-    const email = form["mail"];
-    const password = form["mdp"];
-    const confirmPassword = form["confirmMdp"];
-    
+  //selection du formulaire
+  const form = document.forms["formulaire"];
+  //recup des champs
+  const userName = form["userName"];
+  const email = form["mail"];
+  const password = form["mdp"];
+  const confirmPassword = form["confirmMdp"];
 
-    const errorUsername = document.getElementById("error-username");
-    const errorMail = document.getElementById("error-mail");
-    const errorMdp = document.getElementById("error-mdp");
-    const errorConfirm = document.getElementById("error-confirm");
-    
-// event on clique sur confirmer (submit)
-form.addEventListener("submit", function (event) {
-//on se dis que le formulaire est valide et après on mettra les conditions false si y'a erreurs
-let formConfirm = true;
+  const errorUsername = document.getElementById("error-username");
+  const errorMail = document.getElementById("error-mail");
+  const errorMdp = document.getElementById("error-mdp");
+  const errorConfirm = document.getElementById("error-confirm");
 
-// reset le formulaire à chaque soumission
-        errorUsername.textContent = "";
-        errorMail.textContent = "";
-        errorMdp.textContent = "";
-        errorConfirm.textContent = "";
+//pour voir le mot de passe (mais ca ne fonctionne qu'à moitié misère)
 
-//pour enlever les blancs, espaces etc et on regarde si c'est pas vide ou invalide
-        const valeurName = userName.value.trim();
-        const valeurMail = email.value.trim();
-        const valeurMdp = password.value.trim();
-        const valeurConfirm = confirmPassword.value.trim();
+const eye = document.querySelector(".iconPasswordOn");
+const eyeoff = document.querySelector(".iconPasswordOff");
+const passwordField = document.querySelector("input[type=password]");
 
-//et c'est parti pour tout vérifier youpi
-//mail
+eye.addEventListener("click", () => {
+   eye.style.display = "none";
+   eyeoff.style.display = "inline";
+  passwordField.type = "text";
+});
 
-if (valeurMail ===""){
-        errorMail.textContent = "Veuillez entrer un email";
-        formConfirm = false;
+eyeoff.addEventListener("click", () => {
+  eyeoff.style.display = "none";
+  eye.style.display = "inline";
+  passwordField.type = "password";
+});
+
+  // event on clique sur confirmer (submit)
+  form.addEventListener("submit", function (event) {
+    //on se dis que le formulaire est valide et après on mettra les conditions false si y'a erreurs
+    let formConfirm = true;
+
+    // reset le formulaire à chaque soumission
+    errorUsername.textContent = "";
+    errorMail.textContent = "";
+    errorMdp.textContent = "";
+    errorConfirm.textContent = "";
+
+    //pour enlever les blancs, espaces etc et on regarde si c'est pas vide ou invalide
+    const valeurName = userName.value.trim();
+    const valeurMail = email.value.trim();
+    const valeurMdp = password.value.trim();
+    const valeurConfirm = confirmPassword.value.trim();
+
+    //et c'est parti pour tout vérifier youpi
+    //mail
+
+    if (valeurMail === "") {
+      errorMail.textContent = "Veuillez entrer un email";
+      formConfirm = false;
     }
     //verifie email valide
-else if (!email.validity.valid) {
-        errorMail.textContent = "Veuillez entrer un email valide";
-        formConfirm = false;
+    else if (!email.validity.valid) {
+      errorMail.textContent = "Veuillez entrer un email valide";
+      formConfirm = false;
     }
 
-//nom utilisateur
-if (valeurName ==="") {
-        errorUsername.textContent = "Veuillez entrer un nom d'utilisateur";
-        formConfirm = false;
-
-    }
-    else if (valeurName.length < 3) {
-        errorUsername.textContent = "Au moins 3 caractères requis";
-        formConfirm = false;       
+    //nom utilisateur
+    if (valeurName === "") {
+      errorUsername.textContent = "Veuillez entrer un nom d'utilisateur";
+      formConfirm = false;
+    } else if (valeurName.length < 3) {
+      errorUsername.textContent = "Au moins 3 caractères requis";
+      formConfirm = false;
     }
 
-//mdp + confirmer
+    //mdp + confirmer
     const nbrMdp = /[\d]/.test(valeurMdp);
     const mdpCaraSpe = /[\W_]/.test(valeurMdp);
     const mdpCara = valeurMdp.length >= 6;
-    
-if (valeurMdp ==="" || valeurConfirm === "") {
-        errorMdp.textContent = "Veuillez entrer un mot de passe";
-        formConfirm = false;
-}
-else if(!nbrMdp || !mdpCaraSpe || !mdpCara) {
-        errorMdp.textContent = "Min. 6 caractères, 1 chiffre, 1 caractère spécial";
-        formConfirm = false;
-}
-//identiques ou pas
-if (valeurMdp !== valeurConfirm) {
-    errorConfirm.textContent = "Les mots de passe ne correspondent pas";
-        formConfirm = false;
-}
-   
-//empeche l'envoi si ca ne fonctionne pas
-if (!formConfirm){
-    event.preventDefault();
-}
-if (formConfirm) {
 
-    //pour stock les différents profils sans écraser le précédent
-    //objet qui représente l'utilisateur
-   
-const newUser = {"username": valeurName,
-    "mail": valeurMail,
-    "password": valeurMdp,
+    
 
-};
-//zou dans le localStorage, ils se différencient avec chaque nom entré
-localStorage.setItem(valeurName, JSON.stringify(newUser));
-  
- //on met event prevent pour que la page ne quitte pas    
-event.preventDefault();
-alert("Inscription terminée !"); 
+    if (valeurMdp === "" || valeurConfirm === "") {
+      errorMdp.textContent = "Veuillez entrer un mot de passe";
+      formConfirm = false;
+    } else if (!nbrMdp || !mdpCaraSpe || !mdpCara) {
+      errorMdp.textContent =
+        "Min. 6 caractères, 1 chiffre, 1 caractère spécial";
+      formConfirm = false;
+    }
+
+   
+    //identiques ou pas
+    if (valeurMdp !== valeurConfirm) {
+      errorConfirm.textContent = "Les mots de passe ne correspondent pas";
+      formConfirm = false;
+    }
+   
+    /*let strength = 0;
+    if (valeurMdp(/[a-z]/) && valeurMdp > 6){
+      strength++; }
     
-form.reset();
-        }
-        
-    });
+    if (valeurMdp(/[\d]/)) {
+      strength++;
+    }
+    if (valeurMdp(/[\W_]/)){
+      strength++;
+    }
+
+    if(strength < 2){
+      barre.style.color ="red";
+    }*/
+
     
+      if (!formConfirm) {
+        //empeche l'envoi si ca ne fonctionne pas
+        event.preventDefault();
+      }
+    if (formConfirm) {
+      //pour stock les différents profils sans écraser le précédent
+      //objet qui représente l'utilisateur
+
+      const newUser = {
+        username: valeurName,
+        mail: valeurMail,
+        password: valeurMdp,
+      };
+      //zou dans le localStorage, ils se différencient avec chaque nom entré
+      localStorage.setItem(valeurName, JSON.stringify(newUser));
+
+      //on met event prevent pour que la page ne quitte pas
+      event.preventDefault();
+      alert("Inscription terminée !");
+
+      form.reset();
+    }
+  });
 }
-entryForm()
+entryForm();
--- memory.js
+++ memory.js
@@ -1,17 +1,32 @@
 const imagesJeu = [
-    "uguette.webp",
-    "sani.webp",
-    "pochi1.webp",
-    "milo.webp",
-    "lulo.webp",
-    "fleur.webp"
+  { src: "uguette.webp", alt: "photo d'Uguette" },
+  { src: "sani.webp", alt: "photo de Sani" },
+  { src: "pochi1.webp", alt: "photo de Pochi" },
+  { src: "milo.webp", alt: "photo de Milo" },
+  { src: "lulo.webp", alt: "photo de Lulo" },
+  { src: "fleur.webp", alt: "photo de Fleur" },
+  
 ];
 
+//tableau alternatives textes aux images
+
+/*const altImages = [
+ "photo d'Uguette",
+ "photo de Sani",
+ "photo de Pochi",
+ "photo de Milo",
+ "photo de Lulo",
+ "photo de Fleur",
+]*/
+//si l'image 1 est selectionnée alors on affiche ce texte
+//ne fonctionne pas car on duplique les cartes
 
 //dupliquer les cartes et le mélanger
 //... : opérateur qui décompose un tableau pour dupliquer, en gros ca transforme en deux imagesJeu
 //sort trie les elements, de maniere perso avec math random et on multiplie par 0.5 pour mélanger de maniere aleatoire
-let imagesMelangees = [...imagesJeu, ...imagesJeu].sort(() => 0.5 - Math.random());
+let imagesMelangees = [...imagesJeu, ...imagesJeu].sort(
+  () => 0.5 - Math.random()
+);
 
 //on déclare
 let cartesRetourne = 0;
@@ -21,89 +36,86 @@ let coups = 0;
 const compteurCoups = document.getElementById("coups"); //on prend l'element coups dans le html
 let pairesTrouvees = 0;
 
-
 //selection de la zone de jeu dans le html
-const zoneJeu = document.querySelector('.jeu');
-
+const zoneJeu = document.querySelector(".jeu");
 
 //espace pour relancer
-document.addEventListener('keydown', function (event) { 
-if (event.code === 'Space' ){
-    window.location.reload(); }
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Space") {
+    window.location.reload();
+  }
 });
 
 // pour chaque image mélangée on crée une carte (div)
-imagesMelangees.forEach((image) => {
-    const carte = document.createElement("div");
-    carte.classList.add("carte");
-    
-//on ajoute le html dans carte avec les deux faces et patati patata    
-carte.innerHTML = `
+
+imagesMelangees.forEach((carteData) => {
+  const carte = document.createElement("div");
+  carte.classList.add("carte");
+
+  //on ajoute le html dans carte avec les deux faces et patati patata
+  carte.innerHTML = `
         <div class="flip-box-inner">
             <div class="flip-box-front"></div>
             <div class="flip-box-back">
-                <img src="imagesMemory/${image}" alt="image">
+                <img src="imagesMemory/${carteData.src}" alt="${carteData.alt}">
             </div>
         </div>
     `;
 
-//flip box inner contient les deux faces
-//front c'est la face avant
-//le dos et donc l'image
-// img src, image ajoutée à l'arrière de la carte
+  //flip box inner contient les deux faces
+  //front c'est la face avant
+  //le dos et donc l'image
+  // img src, image ajoutée à l'arrière de la carte
 
+  //on stocke le nom de l'image pour comparer
+  //dataset pour lire les données html des cartes (juste dessus)
+  carte.dataset.image = carteData.src;
 
-//on stocke le nom de l'image pour comparer
-//dataset pour lire les données html des cartes (juste dessus)
-    carte.dataset.image = image;
-
-    //on ajoute le clic de la carte
-    carte.addEventListener("click", () => {
-
+  //on ajoute le clic de la carte
+  carte.addEventListener("click", () => {
     //pour éviter les bugs si on clique plusueirus fois sur la même ou si on essaye de clic sur une 3e
     //en gros on return si la carte est déjà flip ou si y'en a 2 retournées
-   if(carte.classList.contains("flipped") || cartesRetourne === 2) return;
+    if (carte.classList.contains("flipped") || cartesRetourne === 2) return;
 
-   //on retourne la carte
-   carte.classList.add("flipped");
-   cartesRetourne++;
+    //on retourne la carte
+    carte.classList.add("flipped");
+    cartesRetourne++;
 
-   //on attribu les valeurs pour chaque carte retournée
-if(cartesRetourne === 1){
-    carteUne = carte;
-} else if (cartesRetourne === 2){
-    carteDeux = carte;
-    coups++;
-    compteurCoups.textContent = coups; //maj du compteur de coups
+    //on attribu les valeurs pour chaque carte retournée
+    if (cartesRetourne === 1) {
+      carteUne = carte;
+    } else if (cartesRetourne === 2) {
+      carteDeux = carte;
+      coups++;
+      compteurCoups.textContent = coups; //maj du compteur de coups
 
-//vérifier si les deux cartes sont identiques
-setTimeout(() => {
-//si ca correspond
-    if (carteUne.dataset.image === carteDeux.dataset.image) {
-        pairesTrouvees++;
-        if (pairesTrouvees === imagesJeu.length) {
-             alert("Bravo ! Vous avez réussi en : " + coups);
-        } 
-        //si non, elles se retournent
-    }else {
-        carteUne.classList.remove("flipped");
-        carteDeux.classList.remove("flipped");
+      //vérifier si les deux cartes sont identiques
+      setTimeout(() => {
+        //si ca correspond
+        if (carteUne.dataset.image === carteDeux.dataset.image) {
+          pairesTrouvees++;
+          if (pairesTrouvees === imagesJeu.length) {
+            alert("Bravo ! Vous avez réussi en : " + coups);
+          }
+          //si non, elles se retournent
+        } else {
+          carteUne.classList.remove("flipped");
+          carteDeux.classList.remove("flipped");
+        }
+        //on réinitialise pour la suite
+        cartesRetourne = 0;
+        carteUne = null;
+        carteDeux = null;
+      }, 1000);
+        
+      //on stocke le nouveau score dans le localStorage 
+          const newScore = "newScore";
+          localStorage.setItem(newScore, JSON.stringify(coups));
     }
-    //on réinitialise pour la suite
-    cartesRetourne = 0;
-carteUne = null;
-carteDeux = null;
-},
-1000);
-
-
-
-
-}
-});
-zoneJeu.appendChild(carte); //ajout de la carte au DOM
+  });
+  zoneJeu.appendChild(carte); //ajout de la carte au DOM
 });
 
- /*while (function myFunction() {
+/*while (function myFunction() {
   document.getElementById(" ").innerHTML = "You pressed a key inside the input field";
 })*/
